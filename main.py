2d3c058fb0866e1893267c9e757392691f3c0c29
danny10ison <pagesngod@gmail.com>
2023-12-30 15:47:55 +0000

    Add main file

---
+++ main.py
@@ -0,0 +1,36 @@
+"""A simple calculator
+
+More explanation
+"""
+
+# Give a welcome prompt
+# Take user input
+# perform operations
+# display result to user
+
+# variable to hold welcom prompt
+msg = "Simple calculator"
+
+# check the type of the variable
+# print(type(msg))
+print(msg)
+
+num1 = int(input("Enter the first number: "))
+operator = input("Enter the operator(+, -, *, /): ")
+num2 = int(input("Enter the second number: "))
+result = 0
+
+
+# compare the operator here
+if operator == '+':
+    # do addition here
+elif operator == '-':
+    # do subtraction here
+elif operator == '*':
+    # do multiplication here
+elif operator == '/':
+    # do division
+
+
+# print("num1")
+print(f"{num1} {operator} {num2} = {result}")
